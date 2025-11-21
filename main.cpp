079992034b281b7c9bae6c3eaf69b3bee2279b8e
Shravan <shravanmaharaj1505@gmail.com>
2025-11-21 13:36:30 -0500

    created main dispatcher

--- main.cpp
+++ main.cpp
@@ -1,159 +1,44 @@
-//
-// Example from: http://www.amparo.net/ce155/sem-ex.c
-//
-// Adapted using some code from Downey's book on semaphores
-//
-// Compilation:
-//
-//       g++ main.cpp -lpthread -o main -lm
-// or 
-//      make
-//
-
-#include <unistd.h>     /* Symbolic Constants */
-#include <sys/types.h>  /* Primitive System Data Types */
-#include <errno.h>      /* Errors */
-#include <stdio.h>      /* Input/Output */
-#include <stdlib.h>     /* General Utilities */
-#include <pthread.h>    /* POSIX Threads */
-#include <string.h>     /* String handling */
-#include <semaphore.h>  /* Semaphore */
+// main.cpp
 #include <iostream>
+#include <cstdlib>
+
 using namespace std;
 
-/*
- This wrapper class for semaphore.h functions is from:
- http://stackoverflow.com/questions/2899604/using-sem-t-in-a-qt-project
- */
-class Semaphore {
-public:
-    // Constructor
-    Semaphore(int initialValue)
-    {
-        sem_init(&mSemaphore, 0, initialValue);
-    }
-    // Destructor
-    ~Semaphore()
-    {
-        sem_destroy(&mSemaphore); /* destroy semaphore */
-    }
-    
-    // wait
-    void wait()
-    {
-        sem_wait(&mSemaphore);
-    }
-    // signal
-    void signal()
-    {
-        sem_post(&mSemaphore);
-    }
-    
-    
-private:
-    sem_t mSemaphore;
-};
+// These will be defined in problem1.cpp, problem2.cpp, etc.
+void run_problem1();
+void run_problem2();
+void run_problem3();
+void run_problem4();
 
-
-
-
-/* global vars */
-const int bufferSize = 5;
-const int numConsumers = 3; 
-const int numProducers = 3; 
-
-/* semaphores are declared global so they can be accessed
- in main() and in thread routine. */
-Semaphore Mutex(1);
-Semaphore Spaces(bufferSize);
-Semaphore Items(0);             
-
-
-
-/*
-    Producer function 
-*/
-void *Producer ( void *threadID )
-{
-    // Thread number 
-    int x = (long)threadID;
-
-    while( 1 )
-    {
-        sleep(3); // Slow the thread down a bit so we can see what is going on
-        Spaces.wait();
-        Mutex.wait();
-            printf("Producer %d adding item to buffer \n", x);
-            fflush(stdout);
-        Mutex.signal();
-        Items.signal();
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        cout << "Usage: cse4001_sync <problem#>" << endl;
+        cout << "  1 = No-starve readers–writers" << endl;
+        cout << "  2 = Writer-priority readers–writers" << endl;
+        cout << "  3 = Dining philosophers solution #1" << endl;
+        cout << "  4 = Dining philosophers solution #2" << endl;
+        return 1;
     }
 
+    int problem = atoi(argv[1]);
+
+    switch (problem) {
+        case 1:
+            run_problem1();
+            break;
+        case 2:
+            run_problem2();
+            break;
+        case 3:
+            run_problem3();
+            break;
+        case 4:
+            run_problem4();
+            break;
+        default:
+            cout << "Invalid problem number: " << problem << endl;
+            return 1;
+    }
+
+    return 0;
 }
-
-/*
-    Consumer function 
-*/
-void *Consumer ( void *threadID )
-{
-    // Thread number 
-    int x = (long)threadID;
-    
-    while( 1 )
-    {
-        Items.wait();
-        Mutex.wait();
-            printf("Consumer %d removing item from buffer \n", x);
-            fflush(stdout);
-        Mutex.signal();
-        Spaces.signal();
-        sleep(5);   // Slow the thread down a bit so we can see what is going on
-    }
-
-}
-
-
-int main(int argc, char **argv )
-{
-    pthread_t producerThread[ numProducers ];
-    pthread_t consumerThread[ numConsumers ];
-
-    // Create the producers 
-    for( long p = 0; p < numProducers; p++ )
-    {
-        int rc = pthread_create ( &producerThread[ p ], NULL, 
-                                  Producer, (void *) (p+1) );
-        if (rc) {
-            printf("ERROR creating producer thread # %d; \
-                    return code from pthread_create() is %d\n", p, rc);
-            exit(-1);
-        }
-    }
-
-    // Create the consumers 
-    for( long c = 0; c < numConsumers; c++ )
-    {
-        int rc = pthread_create ( &consumerThread[ c ], NULL, 
-                                  Consumer, (void *) (c+1) );
-        if (rc) {
-            printf("ERROR creating consumer thread # %d; \
-                    return code from pthread_create() is %d\n", c, rc);
-            exit(-1);
-        }
-    }
-
-    printf("Main: program completed. Exiting.\n");
-
-
-    // To allow other threads to continue execution, the main thread 
-    // should terminate by calling pthread_exit() rather than exit(3). 
-    pthread_exit(NULL); 
-
-
-} /* main() */
-
-
-
-
-
-
